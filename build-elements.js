c4e292454eb9d2c094a15b48cf33e87b1fff36a6
Nikos Mpakaris <nikos.mpakaris@gmail.com>
2020-12-23 15:14:44 +0100

    build-element.js

--- build-elements.js
+++ build-elements.js
@@ -1,22 +1,22 @@
 const fs = require('fs-extra');
 const concat = require('concat');
 
-const yargs = require('yargs/yargs')
-const { hideBin } = require('yargs/helpers')
-const argv = yargs(hideBin(process.argv)).argv
+const yargs = require('yargs/yargs');
+const { hideBin } = require('yargs/helpers');
+const argv = yargs(hideBin(process.argv)).argv;
 
-let appName = argv.app
+let appName = 'pricing-calculator-cushions';
+console.log(argv.app);
+console.log(appName);
 
+(async function build() {
+  const files = [
+    './dist/pricing-calculator-cushions/runtime.js',
+    './dist/pricing-calculator-cushions/polyfills.js',
+    './dist/pricing-calculator-cushions/scripts.js',
+    './dist/pricing-calculator-cushions/main.js',
+  ];
 
-
-  (async function build() {
-    const files = [
-      `./dist/${appName}/runtime.js`,
-      `./dist/${appName}/polyfills.js`,
-      `./dist/${appName}/scripts.js`,
-      `./dist/${appName}/main.js`,
-    ];
-
-    await fs.ensureDir('elements');
-    await concat(files, 'elements/' + appName + '.js');
-  })();
+  await fs.ensureDir('elements');
+  await concat(files, `elements/${appName}.js`);
+})();
--- test.js
+++ test.js
@@ -1,2 +1,6 @@
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
 
+console.log(argv.app)
 
