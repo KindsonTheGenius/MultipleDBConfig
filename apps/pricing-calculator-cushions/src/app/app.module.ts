a72cbaf1e2e03deb19587fb5274a33bea0aa4bc3
Nikos Mpakaris <nikos.mpakaris@gmail.com>
2020-12-23 10:36:36 +0100

    bootstrapped Pricing-Calculator-Cushions and created Build-Scripts

--- apps/pricing-calculator-cushions/src/app/app.module.ts
+++ apps/pricing-calculator-cushions/src/app/app.module.ts
@@ -1,8 +1,14 @@
-import { NgModule } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeDe from '@angular/common/locales/de';
+import { Injector, LOCALE_ID, NgModule } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { CushionCalculatorModule } from '../../../../libs/cushion-calculator/src';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
+
+registerLocaleData(localeDe, 'de');
 
 @NgModule({
   declarations: [AppComponent],
@@ -11,7 +17,20 @@ import { AppComponent } from './app.component';
     RouterModule.forRoot([], { initialNavigation: 'enabled' }),
     CushionCalculatorModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent],
+  providers: [
+    {
+      provide: LOCALE_ID,
+      useValue: 'de-DE',
+    },
+  ],
+  bootstrap: environment.production ? [] : [AppComponent],
+  entryComponents: [AppComponent],
 })
-export class AppModule {}
+export class AppModule {
+  constructor(private injector: Injector) {
+    const el = createCustomElement(AppComponent, { injector });
+    customElements.define('pricing-calculator-cushions', el);
+  }
+
+  ngDoBootstrap() {}
+}
--- build-elements.js
+++ build-elements.js
@@ -1,14 +1,22 @@
 const fs = require('fs-extra');
 const concat = require('concat');
 
-(async function build() {
-  const files = [
-    './dist/pricing-calculator/runtime.js',
-    './dist/pricing-calculator/polyfills.js',
-    './dist/pricing-calculator/scripts.js',
-    './dist/pricing-calculator/main.js'
-  ];
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
+const argv = yargs(hideBin(process.argv)).argv
 
-  await fs.ensureDir('elements');
-  await concat(files, 'elements/pricing-calculator.js');
-})();
+let appName = argv.app
+
+
+
+  (async function build() {
+    const files = [
+      `./dist/${appName}/runtime.js`,
+      `./dist/${appName}/polyfills.js`,
+      `./dist/${appName}/scripts.js`,
+      `./dist/${appName}/main.js`,
+    ];
+
+    await fs.ensureDir('elements');
+    await concat(files, 'elements/' + appName + '.js');
+  })();
